48acaf186ed54702a2a00a3a6af371f825dee838
John Nevin <john@nevin.dev>
2026-02-02 16:57:10 -0500

    Add build workflow
    Create build workflow for GitHub Pages.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<div class="mt-20 flex flex-col items-center text-center">
+	<h1 class="text-2xl font-black">404</h1>
+	<p class="text-xl font-semibold">Page not found</p>
+	<h4 class="text-lg font-normal">Head back to</h4>
+	<div class="my-4 flex max-w-md flex-row justify-between space-x-6">
+		<a class="rounded-2xl bg-red-500 px-8 py-2 text-white hover:bg-red-700" href="/map"
+			>Auto Report</a
+		>
+		<a class="rounded-2xl bg-blue-500 px-8 py-2 text-white hover:bg-blue-700" href="/map">Map</a>
+		<a class="hover:orange-700 rounded-2xl bg-orange-500 px-8 py-2 text-white" href="/map"
+			>My Reports</a
+		>
+	</div>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,12 @@ const config = {
   ],
 
 	kit: {
-		adapter: adapter({fallback: '200.html'})
+		adapter: adapter({
+      fallback: '404.html'
+    }),
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    }
 	},
 
 	extensions: ['.svelte', '.svx']
